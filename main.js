e2f198355128ca7069318f9d58afd8ef2e74c67e
Ivy Nguyen <animeweirdo24@gmail.com>
2022-05-25 18:13:52 -0600

    Add query selectors and event listeners to main.js. Add hidden element in the paragraph tag. Add hidden css properties

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 var recieveMessageButton = document.querySelector('button')
 
+var bellImage = document.querySelector('img')
+
+
+
 recieveMessageButton.addEventListener('click', )
+
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}
+
+function recieveMessage() {
+
+bellImage.classList.add('hidden')
+}
