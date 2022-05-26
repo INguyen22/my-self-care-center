34b4652cc3ab61176663e806b640594a772cfd8d
Ivy Nguyen <animeweirdo24@gmail.com>
2022-05-25 20:22:33 -0600

    Edit radio input in html

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
 var recieveMessageButton = document.querySelector('button')
 
+var affirmationInput = document.getElementById('affirmation')
+
+
 var bellImage = document.querySelector('img')
+var paragraph = document.querySelector('p')
 
 
-
-recieveMessageButton.addEventListener('click', )
+recieveMessageButton.addEventListener('click', recieveMessage)
 
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
 
-function recieveMessage() {
+function recieveMessage(event) {
+event.preventDefault()
+//p.innerText =
 
 bellImage.classList.add('hidden')
+paragraph.classList.remove('hidden')
 }
