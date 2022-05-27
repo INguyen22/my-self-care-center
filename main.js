3c2f79ed44bae484298bb6eeaefa051fafc36cda
Ivy Nguyen <animeweirdo24@gmail.com>
2022-05-26 18:17:09 -0600

    Add script source to html body. Make the recieve message button produce a random quote depending on if they want an affirmation or mantra. Make it so that users can't recieve a message if they haven't selected an option

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var recieveMessageButton = document.querySelector('button')
 
 var affirmationInput = document.getElementById('affirmation')
+var mantraInput = document.getElementById('mantra')
 
 
 var bellImage = document.querySelector('img')
@@ -15,8 +16,17 @@ function getRandomIndex(array) {
 
 function recieveMessage(event) {
 event.preventDefault()
-//p.innerText =
+if (affirmationInput.checked === true) {
+  paragraph.innerText = affirmation[getRandomIndex(affirmation)]
 
-bellImage.classList.add('hidden')
-paragraph.classList.remove('hidden')
+  bellImage.classList.add('hidden')
+  paragraph.classList.remove('hidden')
+}
+else if (mantraInput.checked === true) {
+  paragraph.innerText = mantra[getRandomIndex(mantra)]
+  bellImage.classList.add('hidden')
+  paragraph.classList.remove('hidden')
+}
+else
+alert("Please select what kind of message you want")
 }
